6548d554ae2de575458fc708d9924caf5df7a93f
Devendra2722001 <devendra.dabhi@volansys.com>
2022-05-10 15:12:48 +0530

    Checkout now working

--- src/component/Checkout.jsx
+++ src/component/Checkout.jsx
@@ -56,6 +56,9 @@ const Checkout = () => {
             button: "Okay!",
           });
           history.push("/");
+          setTimeout(() => {
+              window.location.reload()
+          }, 2000);
           //
     }
 
