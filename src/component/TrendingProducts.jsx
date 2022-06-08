f397136c9b74901a01c1a533646108b125de6588
Devendra2722001 <devendra.dabhi@volansys.com>
2022-06-08 14:46:56 +0530

    Demo Backup

--- src/component/TrendingProducts.jsx
+++ src/component/TrendingProducts.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+
 
 const TrendingProducts = () => {
     return (
@@ -9,30 +11,39 @@ const TrendingProducts = () => {
             <div className="TrendingProductsCards"> 
 
                                 <div className="card_Trending">
-                                    <div className="Big-card_Trending">                                    
-                                    </div>
+                                <Link to={`/productlist/628f296dd516b474a4f17f0f`} id="copyright"><div className="Big-card_Trending">                                    
+                                    </div></Link>
+                                    
                                 </div>
 
                             <div className="gallery">
                                 <div className="card_Trending_Small1">
+                                <Link to={`/productlist/6284f2578720fb52cc1f18e3`} id="copyright">
                                     <div className="card-inner_Trending1">
                                     
                                     </div>
+                                </Link>
                                 </div>
                                 <div className="card_Trending_Small2">
+                                <Link to={`/productlist/628362bc7df7b318cd3b7565`} id="copyright">
                                     <div className="card-inner_Trending2">
                                     
                                     </div>
+                                </Link>
                                 </div>
                                 <div className="card_Trending_Small3" id="MT50">
+                                <Link to={`/productlist/62835f1b7df7b318cd3b7553`} id="copyright">
                                     <div className="card-inner_Trending3">
                                     
                                     </div>
+                                </Link>
                                 </div>
                                 <div className="card_Trending_Small4" id="MT50">
+                                <Link to={`/productlist/62836838568cb7a3e23f7126`} id="copyright">
                                     <div className="card-inner_Trending4">
                                     
                                     </div>
+                                </Link>
                                 </div>
                             </div>
 
